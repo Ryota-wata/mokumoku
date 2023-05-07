a3be344f7684612c60779ab3ef878bed6818e698
Ryota <disneyresort1983@icloud.com>
2023-05-07 21:41:05 +0900

    試験課題実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def follows
+    @follow_users = current_user.follow_users
+  end
+
   def user_params
     params.require(:user).permit(:email, :name, :password, :password_confirmation)
   end
